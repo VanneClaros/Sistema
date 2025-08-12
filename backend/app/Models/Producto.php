b9ad839e8d4e1c3f2a15eb75334519709afec4a5
vane <vanessaclaros97@hotmail.com>
2025-08-12 18:09:45 -0400

    Creacion de Modelos

---
+++ backend/app/Models/Producto.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Producto
+ *
+ * @property $id
+ * @property $categoria_id
+ * @property $nombre
+ * @property $precio
+ * @property $cantidad
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @property Categoria $categoria
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Producto extends Model
+{
+    
+    protected $perPage = 20;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['categoria_id', 'nombre', 'precio', 'cantidad'];
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function categoria()
+    {
+        return $this->belongsTo(\App\Models\Categoria::class, 'categoria_id', 'id');
+    }
+    
+}
